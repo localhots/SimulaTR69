d05cdc666cf1ea51e299fddbbc211b3a9ff9dbb5
Gregory Eremin <g@erem.in>
2024-08-04 22:47:54 +0200

    Basic documentation

--- README.md
+++ README.md
@@ -1,3 +1,67 @@
 # TR-069 simulator
 
-WIP
+A CPE simulator for TR-069 protocol. It supports both TR-098 and TR-181
+datamodel formats.
+
+> [!WARNING]
+> This software is a work in progress. Implementation of TR-069 is incomplete
+and it might not behave correctly in all scenarios.
+
+# Installation
+
+Give it a serial number, point to a datamodel file and the ACS and it should
+work out of the box.
+
+See `device/server/config.go` for available configuration options.
+
+## Docker Compose
+
+Add a service to the `docker-compose.yml` file:
+```yaml
+services:
+  tr069sim:
+    image: ghcr.io/localhots/simulatr69:latest
+    volumes:
+      - /path/to/datamodel.csv:/app/datamodel.csv
+      - sim_state:/state
+    ports:
+      - 7547
+    environment:
+      SERIAL_NUMBER: G3000E-9799109101
+      DATAMODEL_PATH: /app/datamodel.csv
+      STATE_PATH: /state/state.json
+      ACS_URL: http://myisp/acs
+      LOG_LEVEL: debug
+
+volumes:
+  sim_state:
+```
+
+# Use
+
+All required methods are supported and should behave realistically, except
+Upload and Factory Reset currently don't do anything. There are quirks though.
+
+## Connection Requests
+
+All connection requests are accepted and trusted. This is done for convenience.
+If the ACS will set the following parameter values they will be ignored:
+* `ManagementServer.ConnectionRequestUsername`
+* `ManagementServer.ConnectionRequestPassword`
+
+## Firmware Upgrades
+
+The simulator supports firmware upgrades in a simple JSON format:
+```json
+{"version": "123.45"}
+```
+
+If the provided URL can't be loaded or if the firmware file has a different
+format the simulator will respond with a fault.
+
+If everything is fine the simulator will change `DeviceInfo.SoftwareVersion`
+parameter value in its state and pretend to take time to upgrade and reboot.
+
+# License
+
+[MIT](LICENSE)
--- device/server/config.go
+++ device/server/config.go
@@ -20,18 +20,40 @@ const (
 var ErrNoCreds = errors.New("username/password missing")
 
 var Config struct {
-	Host          string        `env:"API_HOST"`
-	Port          uint16        `env:"API_PORT, default=7547"`
-	ACSURL        string        `env:"ACS_URL, required"`
-	SerialNumber  string        `env:"SERIAL_NUMBER, required"`
-	DataModelPath string        `env:"DATAMODEL_PATH, required"`
-	StateFilePath string        `env:"STATE_PATH, default=state.json"`
-	UpgradeDelay  time.Duration `env:"UPGRADE_DELAY, default=15s"`
-	ACSAuth       string        `env:"ACS_AUTH, default=none"`
-	ACSUsername   string        `env:"ACS_USERNAME"`
-	ACSPassword   string        `env:"ACS_PASSWORD"`
-	LogLevel      string        `env:"LOG_LEVEL, default=info"`
+	// Host is the host name or IP address used by the simulator to accept
+	// connection requests. If no value is provided it will be automatically
+	// resolved.
+	Host string `env:"API_HOST"`
+	// Port defines the port number used by the simulator to accept connection
+	// requests.
+	Port uint16 `env:"API_PORT, default=7547"`
+	// ACSURL is the URL for the ACS.
+	ACSURL string `env:"ACS_URL, required"`
+	// SerialNumber will overwrite the DeviceInfo.SerialNumber datamodel
+	// parameter value.
+	SerialNumber string `env:"SERIAL_NUMBER, required"`
+	// DataModelPath must point to a datamodel file in CSV format.
+	DataModelPath string `env:"DATAMODEL_PATH, required"`
+	// StateFilePath points to the state file. If the file doesn't exist it will
+	// be created and will maintain all changes made to the datamodel. Missing
+	// state file will trigger a BOOTSTRAP inform event.
+	StateFilePath string `env:"STATE_PATH, default=state.json"`
+	// UpgradeDelay defines how long the simulator should wait and drop incoming
+	// connection requests to pretend that software upgrades take time.
 	RebootDelay time.Duration `env:"REBOOT_DELAY, default=5s"`
+	// RebootDelay defines how long the simulator should wait and drop incoming
+	// connection requests to pretend that it reboots.
+	UpgradeDelay time.Duration `env:"UPGRADE_DELAY, default=15s"`
+	// ACSAuth configures authentication scheme for the ACS. It defaults to
+	// "none". Supported values: digest, none
+	ACSAuth string `env:"ACS_AUTH, default=none"`
+	// ACSUsername is used to authenticate requests to the ACS.
+	ACSUsername string `env:"ACS_USERNAME"`
+	// ACSPassword is used to authenticate requests to the ACS.
+	ACSPassword string `env:"ACS_PASSWORD"`
+	// LogLevel controls how verbose the levels are. Supported values: trace,
+	// debug, info, warn, error, fatal, panic.
+	LogLevel string `env:"LOG_LEVEL, default=info"`
 }
 
 func LoadConfig(ctx context.Context) error {
